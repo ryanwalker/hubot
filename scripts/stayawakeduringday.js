037593b078e5faf300d10bd2fe56f5f030a4e64a
Ryan Walker <ryanwalker7@gmail.com>
2015-10-16 22:15:15 -0700

    Winston, stay awake during day.

--- scripts/stayawakeduringday.js
+++ scripts/stayawakeduringday.js
@@ -1,15 +1,30 @@
-module.exports = function(robot) {
-  robot.hear(/tim/i, function(msg){
-    msg.reply(time());
-  });
-}
+while (true) {
 
-function time() {
   var today = new Date();
   var hour = today.getUTCHours() - 7;
   if (hour < 0) {
     hour = hour + 24;
   }
-  return hour + ":" + today.getMinutes();
+
+  //Stay awake from 6 AM to 10 PM
+  if (6 < hour && hour < 22) {
+    sleepFor10();
+    ping();
+  } else {
+    sleepFor10()
+    setTimeout(time, 10 * 60 * 1000)
+  }
+
 }
 
+function sleepFor10() {
+  setTimeout(function() {
+  // Just sleeping
+  }, 10 * 60 * 1000)
+}
+
+function ping() {
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://infusionsoft-hubot.herokuapp.com", true);
+  request.send();
+}
