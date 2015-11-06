983b7d3b413b42383aafd83d6687fd7bdefb9c19
Ryan Walker <ryanwalker7@gmail.com>
2015-11-06 10:36:36 -0700

    table flip

---
+++ scripts/table_flip.js
@@ -0,0 +1,25 @@
+module.exports = function(robot) {
+  robot.hear(/table flip/i, function(msg){
+    msg.reply(flipit());
+  });
+}
+
+function flipit() {
+  var flips = [
+    "(╯°□°）╯︵ ┻━┻",
+    "‎(ﾉಥ益ಥ）ﾉ﻿ ┻━┻",
+    "(/ .□.)\ ︵╰(゜Д゜)╯︵ /(.□. \)",
+    "(._.) ~ ︵ ┻━┻",
+    "ʕノ•ᴥ•ʔノ ︵ ┻━┻",
+    "(/¯◡ ‿ ◡)/¯ ~ ┻━┻"
+  ];
+
+  var zeroToFive = getRandomInt(0, 5);
+
+  return flips[zeroToFive];
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
